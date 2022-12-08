b50152cf4622648c01b74dbb8392bad4e3e076ea
p.v.siva sai <painampuramvenkatasivasai@gmail.com>
2022-12-08 20:53:10 +0530

    task-08

---
+++ task-08/index.js
@@ -0,0 +1,7 @@
+let sound1= function(){document.getElementById("audio1").play()};
+let sound2= function(){document.getElementById("audio2").play()};
+let sound3= function(){document.getElementById("audio3").play()};
+let sound4= function(){document.getElementById("audio4").play()};
+let sound5= function(){document.getElementById("audio5").play()};
+let sound6= function(){document.getElementById("audio6").play()};
+let sound7= function(){document.getElementById("audio7").play()};
